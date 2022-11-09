9a75ed7241946160647e49e8c5aa7565eb676879
Mehul Patel <mehulthummar44@gmail.com>
2022-11-09 19:19:09 +0530

    Image URL and Name scrap from the page

--- service/pageScraper.js
+++ service/pageScraper.js
@@ -4,12 +4,29 @@ const scraperObject = {
 		let page = await browser.newPage();
 		await page.goto(this.url);
         await page.waitForSelector('div.s-title-instructions-style');
+
+        
+        let TestData = await page.$$eval("[data-csa-c-type='item']", (product) => {
+
+            let image = product.map(el => el.querySelector("div.s-card-container > div > div.s-product-image-container > span > a > div > img").src);
+            let name = product.map(el => el.querySelector("div.s-card-container > div > div.s-product-image-container > span > a > div > img").alt);
+
+            // let price = product.map(el => el.querySelector("div.s-card-container > div > div.a-section > div.s-price-instructions-style > ")?.innerHTML);
+
+            return {
+                image: image,
+                name: name
+                // price: price
+            };
+        })
+
+        console.log(TestData)
+        
         const products = await page.$$eval("div.s-title-instructions-style", product => {
             product = product.map(el => el.querySelector("h2 > a > span").innerHTML);
             return product;
         })
-        console.log(products)
-		
+        
 
         for (let index = 0; index < products.length; index++) {
             const product = products[index];
